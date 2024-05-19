0ffcc97951d1a3dcec79e4e28b93f7845c05d7f0
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2024-05-20 00:39:18 +0900

    :sparkles: Feat: 엔티티 설계, 매핑

---
+++ src/main/java/umc/spring/domain/Store.java
@@ -0,0 +1,34 @@
+package umc.spring.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String address;
+
+    private Float score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Review> reviewList = new ArrayList<>();
+}
